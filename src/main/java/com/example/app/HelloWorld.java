164c8f05c26cc98e5f2ae1ba95711f8280bb60d6
root <root@panda0071.mylabserver.com>
2016-10-15 01:44:39 +0000

    print hello java

--- src/main/java/com/example/app/HelloWorld.java
+++ src/main/java/com/example/app/HelloWorld.java
@@ -3,7 +3,7 @@ package com.example.app;
 public class HelloWorld{
 		public static void main(String argv[]){
 			System.out.println("Hello World");
-
+			System.out.println("Hello Java users");
 		}					
 
 }
